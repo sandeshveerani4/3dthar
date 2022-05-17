e983ef26978d68345ec5bcbecbb474efe1741b00
sandeshveerani4 <sandeshveerani4@gmail.com>
2022-05-17 13:42:26 +0500

    master

---
+++ 3dthar.js
@@ -0,0 +1,20 @@
+function Clickcontact(){
+    window.scrollTo({
+        top:document.getElementsByClassName('site-footer')[0].offsetTop,
+        behavior:"smooth"
+    })
+}
+
+function ClickTeam(){
+    window.scrollTo({
+        top: document.getElementsByClassName('team-area')[0].offsetTop,
+        behavior:"smooth"
+    })
+}
+
+function ClickMission(){
+    window.scrollTo({
+        top:document.getElementsByClassName('header-extrediv')[0].offsetTop,
+        behavior:"smooth"
+    })
+}
